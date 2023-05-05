e88a941a1950ea01866b8a15a22dfba1a90d18fb
Osamu1908 <midsiberia1908@outlook.jp>
2023-05-05 10:37:29 +0900

    maintanable test suite

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+pub use self::app::Application;
 pub use self::settings::Settings;
-
 pub mod app;
 pub mod domain;
 pub mod email_client;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use clap::Parser;
-use sqlx::PgPool;
-use std::net::TcpListener;
 use std::path::PathBuf;
-use zero2prod::app::run;
-use zero2prod::email_client::EmailClient;
 use zero2prod::settings::Settings;
 use zero2prod::trace::{get_subscriber, init_subscriber, stdout, TraceSettings};
+use zero2prod::Application;
 
 #[derive(Debug, Parser)]
 #[command(version)]
@@ -29,26 +26,6 @@ async fn main() -> hyper::Result<()> {
     });
     init_subscriber(subscriber);
 
-    let address = format!("{}:{}", settings.address, settings.port);
-    let listener = TcpListener::bind(&address).expect("failed to bind address");
-    let db_pool = PgPool::connect_lazy_with(settings.database.connect_options());
-    let sender_email = settings
-        .email_client
-        .sender()
-        .expect("invalid sender email address");
-
-    let email_client = EmailClient::new(
-        settings.email_client.base_url,
-        sender_email,
-        settings.email_client.authorization_token,
-        settings.email_client.timeout,
-    );
-    if settings.database.migrate {
-        sqlx::migrate!("./migrations")
-            .run(&db_pool)
-            .await
-            .expect("failed to migrate the database")
-    }
-    tracing::info!("serving on {}", address);
-    run(listener, db_pool, email_client)?.await
+    let server = Application::build(settings)?;
+    server.await
 }
